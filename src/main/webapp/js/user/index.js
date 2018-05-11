078c0782963f525b383f592825a4e522046ad7ba
fyume <454628528@qq.com>
2018-05-11 22:54:32 +0800

    样式相较SSH那个想弄得好看点

--- src/main/webapp/js/user/index.js
+++ src/main/webapp/js/user/index.js
@@ -1,32 +1,39 @@
-var arr = $("#Left #Left_ul li");
-var temp = $("#Left #Left_ul li:last");
-function left_on($this){
-	$($this).attr("onclick","left_off(this)");
-	var leftOn = window.setInterval("liMove(0)",300);
-	var leftOnOff = window.setInterval("window.clearInterval("+leftOn+")",300*(arr.length));
+var li = $("#Right #Right_ul li");
+var last = $("#Right #Right_ul li:last");
+function right_on($this){
+	$($this).attr("onclick","right_off(this)");
+	var rightOn = window.setInterval("liMove(0)",300);
+	var rightOnOff = window.setInterval("window.clearInterval("+rightOn+")",300*(li.length));
 }
 function liMove(num){
 	if(num==0){
-		$.each(arr,function(){
+		$.each(li,function(){
 			if($(this).css("display")=="none"){
 				$(this).css("animation","liMove 1s");
-				$(this).css("animation-fill-mode","forwards");
 				$(this).css("display","block");
 				return false;
 			}
 		});
 	}else{
-		temp.css("animation","liLeave 1s");
-		temp.css("animation-fill-mode","backwards");
-		temp.css("display","none");
-		temp = temp.prev();
-		if(temp.html()==undefined){
-			temp = $("#Left #Left_ul li:last");
+		last.css("animation","liLeave 1s");
+		last.css("display","none");
+		last = last.prev();
+		if(last.html()==undefined){
+			last = $("#Right #Right_ul li:last");
 		}
 	}
 }
-function left_off($this){
-	$($this).attr("onclick","left_on(this)");
-	var leftOff = window.setInterval("liMove(1)",100);
-	var leftOnOff = window.setInterval("window.clearInterval("+leftOff+")",100*(arr.length));
+/*function disapear(){
+	last.css("display","none");
+	last = last.prev();
+	if(last.html()==undefined){
+		last = $("#Right #Right_ul li:last");
+	}
+}*/
+function right_off($this){
+	$($this).attr("onclick","right_on(this)");
+	var rightOff = window.setInterval("liMove(1)",100);
+	var rightOnOff = window.setInterval("window.clearInterval("+rightOff+")",100*(li.length));
+	/*var d = window.setInterval("disapear()",100);
+	window.setInterval("window.clearInterval("+d+")",100*(li.length));*/
 }
